84a7e0e6ccfc0517e6cffb7992a4301feb64ee0b
KISHAN KUMAR <kk1085035@gmail.com>
2026-07-30 13:09:33 +0530

    Time: 0 ms (100%), Space: 43.4 MB (28.23%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,21 +1,17 @@
 class Solution {
     public int minimumPushes(String word) {
-       int[] arr = new int[26];
-       for(char a : word.toCharArray()){
-        arr[a- 'a']++ ;
-       }
-       
        int count = 0 , step = 1 , map = 0;
-        for(int i = 25 ; i >= 0 ; i--){
-            if(map == 8){
+       for(char a : word.toCharArray()){
+         if(map == 8){
                 step++ ;
                 map = 0 ;
             }
-          if(arr[i] != 0){
-            count += step ;
+             count += step ;
             map++ ;
-          }
-        }
+       }
+       
+       
+        
         return count ;
     }
 }

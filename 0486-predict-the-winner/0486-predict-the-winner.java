00689ab3d264394d0086c2a0f1f4fb16e4eaf4d3
KISHAN KUMAR <kk1085035@gmail.com>
2026-08-01 09:13:22 +0530

    Time: 0 ms (100%), Space: 42.8 MB (57.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,46 @@
+class Solution {
+    Integer[][] dp;
+
+    public boolean predictTheWinner(int[] nums) {
+        if (nums.length <= 2) return true;
+
+        int sum = 0;
+        int n = nums.length;
+
+        for (int x : nums) {
+            sum += x;
+        }
+
+        dp = new Integer[n][n];
+
+        int score = maxScore(0, n - 1, nums);
+
+        return score >= (sum - score);
+    }
+
+    public int maxScore(int i, int j, int[] nums) {
+        if (i > j) {
+            return 0;
+        }
+
+        if (i == j) {
+            return nums[i];
+        }
+
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+
+        int start = nums[i] + Math.min(
+                maxScore(i + 2, j, nums),
+                maxScore(i + 1, j - 1, nums)
+        );
+
+        int end = nums[j] + Math.min(
+                maxScore(i + 1, j - 1, nums),
+                maxScore(i, j - 2, nums)
+        );
+
+        return dp[i][j] = Math.max(start, end);
+    }
+}

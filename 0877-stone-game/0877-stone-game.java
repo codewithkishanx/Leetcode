bbeb989c576ff4328f0fc2b6134bb0e8464d741c
KISHAN KUMAR <kk1085035@gmail.com>
2026-08-03 01:02:34 +0530

    Time: 0 ms (100%), Space: 43.2 MB (48.3%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,30 +1,6 @@
 class Solution {
-    int[][] dp ;
     public boolean stoneGame(int[] piles) {
-        int n = piles.length ;
-         dp = new int[n][n];
-       int total = 0 ;
-       for(int i :piles){
-        total += i ;
-       } 
-       int alice = max(piles ,0 , piles.length -1) ;
-       return (total - alice)< alice ;
-
-
+        
+        return true;
     }
-    public int  max(int[] x ,int start , int end){
-        if(start > end){
-            return 0 ;
-        }
-        if(start == end){
-            return x[start] ;
-        }
-        if(dp[start][end] != 0){
-            return dp[start][end] ;
-        }
-        int f = x[start] + Math.min(max(x,start+2 ,end),max(x , start +1 ,end -1));
-        int e = x[end] + Math.min(max(x,start+1 ,end-1),max(x , start  ,end -2));
-        return dp[start][end] = Math.max(f,e);
-
-    } 
 }

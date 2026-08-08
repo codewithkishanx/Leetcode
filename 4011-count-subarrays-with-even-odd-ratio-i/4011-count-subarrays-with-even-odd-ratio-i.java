07e210b1f270bb1e692549d5490d35d87892f823
KISHAN KUMAR <kk1085035@gmail.com>
2026-08-08 22:05:15 +0530

    Time: 43 ms (43.41%), Space: 46.4 MB (84.27%) - LeetHub

---
+++ 4011-count-subarrays-with-even-odd-ratio-i/4011-count-subarrays-with-even-odd-ratio-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countRatioSubarrays(int[] nums, int a, int b) {
+        int count = 0 ;
+        int n = nums.length ;
+        for(int i = 0 ; i < n ; i++){
+            int even = 0 , odd = 0 ;
+            for(int j = i ; j<n ; j++){
+                if(nums[j]%2 ==0){
+                    even++ ;
+                }else{
+                    odd++ ;
+                }
+                if (odd > 0 && (long) even * b <= (long) odd * a) {
+    count++;
+}
+            }
+        }
+        return count;
+    }
+}

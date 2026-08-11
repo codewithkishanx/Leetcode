b00d67621a696bad6801806c4ee1e34852ad2746
KISHAN KUMAR <kk1085035@gmail.com>
2026-08-11 19:15:25 +0530

    Time: 1 ms (99.92%), Space: 46.6 MB (44.57%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        boolean[][] hor = new boolean[9][10];
+        boolean[][] ver = new boolean[9][10];
+        boolean[][] box = new boolean[9][10];
+
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+                    continue;
+                }
+
+                int val = board[i][j] - '0';
+
+                if (hor[i][val]) {
+                    return false;
+                }
+                hor[i][val] = true;
+
+  
+                if (ver[j][val]) {
+                    return false;
+                }
+                ver[j][val] = true;
+
+                // Check 3x3 box
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                if (box[boxIndex][val]) {
+                    return false;
+                }
+                box[boxIndex][val] = true;
+            }
+        }
+
+        return true;
+    }
+}
